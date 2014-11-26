48eef91d9c238f52fcf52eba11e1caa3f09ef377
Nick Mathewson <nickm@torproject.org>
2014-11-25 21:08:10 -0500

    Idiot-proof test_streamgen

--- test/test_streamgen.c
+++ test/test_streamgen.c
@@ -1,12 +1,21 @@
 
+#include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <string.h>
 #include "otterylite.h"
 
-int main(int c, char **v)
+int main(int argc, char **argv)
 {
   unsigned char buf[1024];
-  (void)c; (void)v;
+
+  if (argc == 1 || strcmp(argv[1], "--yes-really")) {
+    fprintf(stderr, "Hi! This program is designed to spew an infinite stream "
+            "of random bytes to stdout, for testing. If you just called it "
+            "wondering what it did... that's what. If you really want to blit "
+            "randomness to stderr, say --yes-really\n");
+    return 1;
+  }
 
   while (1) {
     ottery_random_bytes(buf, 1024);
