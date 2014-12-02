ec3e559eedbee21ddabe4c6b92c886b50c83e175
Nick Mathewson <nickm@torproject.org>
2014-12-02 08:43:19 -0500

    Crank up the sample size on the benchmarks

--- bench/bench.c
+++ bench/bench.c
@@ -28,32 +28,32 @@ main(int c, char **v)
   struct timeval tv_start, tv_end, tv_diff;
   u8 block[4096];
   int i;
-  const int N = 10000;
+  const int N = 100000;
   uint64_t ns;
 
   (void)c; (void)v;
 
   gettimeofday(&tv_start, NULL);
-  for (i = 0; i < N; ++i)
+  for (i = 0; i < N*100; ++i)
     {
       ottery_random();
     }
   gettimeofday(&tv_end, NULL);
   timersub(&tv_end, &tv_start, &tv_diff);
   ns = tv_diff.tv_sec * (uint64_t)1000000000 + tv_diff.tv_usec * 1000;
-  ns /= N;
+  ns /= N*100;
   printf("%ld ns per call to ottery_random()\n", (long)ns);
 
 
   gettimeofday(&tv_start, NULL);
-  for (i = 0; i < N; ++i)
+  for (i = 0; i < N*10; ++i)
     {
       ottery_random_buf(block, 1024);
     }
   gettimeofday(&tv_end, NULL);
   timersub(&tv_end, &tv_start, &tv_diff);
   ns = tv_diff.tv_sec * (uint64_t)1000000000 + tv_diff.tv_usec * 1000;
-  ns /= N;
+  ns /= N*10;
   printf("%ld ns per call to ottery_random_buf(1024)\n", (long)ns);
 
   gettimeofday(&tv_start, NULL);
